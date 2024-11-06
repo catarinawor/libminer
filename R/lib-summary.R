a59147da5bf22a3982637300482c9d73547a9387
caw001 <catarina.wor@dfo-mpo.gc.ca>
2024-11-06 08:58:00 -0800

    add vignette

--- R/lib-summary.R
+++ R/lib-summary.R
@@ -1,3 +1,16 @@
+
+#' R Library Summary
+#'
+#' Provides a brief summary of the package libraries on your machine
+#'
+#' @param sizes Should sizes of libraries be calculated. Default `FALSE`.
+#'
+#' @return A data.frame containing the count of packages in each of the user's
+#'   libraries
+#' @export
+#'
+#' @examples
+#' lib_summary()
 lib_summary <- function(sizes = FALSE) {
   if (!is.logical(sizes)) {
     stop("'sizes' must be logical (TRUE or FALSE)")
